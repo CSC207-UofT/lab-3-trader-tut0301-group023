5296759dc39ef44e02893d700dcccfebdd7dbdb4
blur-b <karebear1159@gmail.com>
2021-09-27 15:59:02 -0400

    added Cow object in all_items in main

--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -96,8 +96,9 @@ public class Trader {
         List<Tradable> all_items = Arrays.asList(
                 new Horse(),
                 new Horse(),
-                new Horse()
-                // TODO: Add Tradable objects here!
+                new Horse(),
+                // Add Tradable objects here!
+                new Cow()
         );
 
         /* Below, we've created two Traders. Their money, inventory, and
